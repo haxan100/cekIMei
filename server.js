934424d1555970d440e2ca006baf7c72bb034628
haxans100 <agostand@gmail.com>
2026-09-03 15:58:30 +0700

    push kan !

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static("public"));
 
 function luhnCheck(imei) {
   if (!/^\d{15}$/.test(imei)) return false;
